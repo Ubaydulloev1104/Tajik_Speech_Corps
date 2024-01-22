fdd7d29458dbb84dc658372e025b8ef07343c9aa
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-01-22 22:57:18 +0500

    Add CustomWebApplicationFactory

---
+++ Tests/TSR_Accoun_Tests/Application.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Application.IntegrationTests
+{
+	internal class CustomWebApplicationFactory : WebApplicationFactory<Program>
+	{
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			var inMemoryConfiguration = GetInMemoryConfiguration();
+			builder.ConfigureAppConfiguration(configurationBuilder =>
+			{
+				var integrationConfig = new ConfigurationBuilder()
+					.AddInMemoryCollection(inMemoryConfiguration)
+					.AddEnvironmentVariables()
+					.Build();
+				configurationBuilder.AddConfiguration(integrationConfig);
+			});
+
+			builder.ConfigureTestServices(services =>
+			{
+				var serviceProvider = services.BuildServiceProvider();
+			});
+		}
+
+		IDictionary<string, string> GetInMemoryConfiguration()
+		{
+			Dictionary<string, string> inMemoryConfiguration = new()
+		{
+			{ "UseInMemoryDatabase", "true" }, { "UseFileEmailService", "true" }, { "UseFileSmsService", "true" }
+		};
+			return inMemoryConfiguration;
+		}
+	}
+}
