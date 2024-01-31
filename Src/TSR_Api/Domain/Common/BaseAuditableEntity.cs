22b497bfee5ea5f410f195b1ff5c8188b2e1675d
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-01-31 23:05:51 +0500

    Add BaseAuditableEntity

---
+++ Src/TSR_Api/Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Common
+{
+	public abstract class BaseAuditableEntity : BaseEntity
+	{
+		public DateTime CreatedAt { get; set; }
+
+		public Guid CreatedBy { get; set; }
+
+		public DateTime? LastModifiedAt { get; set; }
+
+		public Guid LastModifiedBy { get; set; }
+	}
+}
