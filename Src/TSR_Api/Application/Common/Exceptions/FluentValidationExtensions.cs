2bbb2a68dc2fbeb9af39d771935976e0935040b6
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-03 21:51:31 +0500

    Add FluentValidationExtensions

---
+++ Src/TSR_Api/Application/Common/Exceptions/FluentValidationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Common.Exceptions
+{
+	public static class FluentValidationExtensions
+	{
+		public static IRuleBuilderOptions<T, TProperty> NotEmptyWithDefaultMessage<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder)
+		{
+			return ruleBuilder.NotEmpty().WithMessage("{PropertyName} should not be empty");
+		}
+	}
+}
