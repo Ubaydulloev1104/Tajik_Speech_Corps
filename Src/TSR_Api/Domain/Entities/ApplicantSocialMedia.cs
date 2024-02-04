a2ec11de220a532c991b448c1eb7d399f6914111
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-04 22:14:57 +0500

    change

---
+++ Src/TSR_Api/Domain/Entities/ApplicantSocialMedia.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+	public class ApplicantSocialMedia : BaseEntity
+	{
+		public string ProfileUrl { get; set; }
+		public SocialMediaType Type { get; set; }
+
+		public Guid UserId { get; set; }
+	}
+}
