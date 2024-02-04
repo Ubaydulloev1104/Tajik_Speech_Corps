b7717fa1f3a8fe71ddffa523adaed42e292791b1
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-04 21:37:26 +0500

    Add ValidationBehaviour

---
+++ Src/TSR_Api/Application/Common/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation.Results;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Common.Behaviours
+{
+	public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+	where TRequest : IRequest<TResponse>
+	{
+		private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+		public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+		{
+			_validators = validators;
+		}
+
+		public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+			CancellationToken cancellationToken)
+		{
+			if (_validators.Any())
+			{
+				ValidationContext<TRequest> context = new ValidationContext<TRequest>(request);
+
+				ValidationResult[] validationResults = await Task.WhenAll(
+					_validators.Select(v =>
+						v.ValidateAsync(context, cancellationToken)));
+
+				List<ValidationFailure> failures = validationResults
+					.Where(r => r.Errors.Any())
+					.SelectMany(r => r.Errors)
+					.ToList();
+
+				if (failures.Any())
+				{
+					throw new ValidationException(failures);
+				}
+			}
+
+			return await next();
+		}
+	}
+}
