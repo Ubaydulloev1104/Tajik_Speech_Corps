aca8496d5874ec92f099335c43323181128e0359
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-04 21:35:18 +0500

    Add PerformanceBehaviour and LoggingBehaviour

---
+++ Src/TSR_Api/Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Security;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Common.Behaviours
+{
+	public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+	{
+		private readonly ICurrentUserService _currentUserService;
+		private readonly ILogger _logger;
+
+		public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService)
+		{
+			_logger = logger;
+			_currentUserService = currentUserService;
+		}
+
+		public async Task Process(TRequest request, CancellationToken cancellationToken)
+		{
+			string requestName = typeof(TRequest).Name;
+			Guid? userId = _currentUserService.GetUserId();
+			string userName = _currentUserService.GetUserName() ?? "";
+			await Task.CompletedTask;
+
+			_logger.LogInformation("TSP Request: {Name} {@UserId} {@UserName} {@Request}",
+				requestName, userId, userName, request);
+		}
+	}
+}
---
+++ Src/TSR_Api/Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using Application.Common.Security;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Application.Common.Behaviours
+{
+	public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+	where TRequest : IRequest<TResponse>
+	{
+		private readonly ICurrentUserService _currentUserService;
+		private readonly ILogger<TRequest> _logger;
+		private readonly Stopwatch _timer;
+
+		public PerformanceBehaviour(
+			ILogger<TRequest> logger,
+			ICurrentUserService currentUserService)
+		{
+			_timer = new Stopwatch();
+
+			_logger = logger;
+			_currentUserService = currentUserService;
+		}
+
+		public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+			CancellationToken cancellationToken)
+		{
+			_timer.Start();
+
+			TResponse response = await next();
+
+			_timer.Stop();
+
+			long elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+			if (elapsedMilliseconds > 500)
+			{
+				string requestName = typeof(TRequest).Name;
+				Guid? userId = _currentUserService.GetUserId();
+				string userName = _currentUserService.GetUserName();
+				_logger.LogWarning(
+					"TSP Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@UserId} {@UserName} {@Request}",
+					requestName, elapsedMilliseconds, userId, userName, request);
+			}
+
+			return response;
+		}
+	}
+}
