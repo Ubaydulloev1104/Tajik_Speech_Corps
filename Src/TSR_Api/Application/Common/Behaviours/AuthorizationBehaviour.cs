1e090b624959f05e73555c3fc3356787cd88c599
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-04 21:33:17 +0500

    add AuthorizationBehaviour

---
+++ Src/TSR_Api/Application/Common/Behaviours/AuthorizationBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Security;
+using MediatR;
+using System.Reflection;
+
+namespace Application.Common.Behaviours
+{
+	public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+	where TRequest : IRequest<TResponse>
+	{
+		private readonly ICurrentUserService _currentUserService;
+		// private readonly IIdentityService _identityService;
+
+		public AuthorizationBehaviour(
+			ICurrentUserService currentUserService)
+		{
+			_currentUserService = currentUserService;
+			// _identityService = identityService;
+		}
+
+		public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+			CancellationToken cancellationToken)
+		{
+			IEnumerable<AuthorizeAttribute> authorizeAttributes =
+				request.GetType().GetCustomAttributes<AuthorizeAttribute>();
+
+			if (authorizeAttributes.Any())
+			{
+				// Must be authenticated user
+				if (_currentUserService.GetUserId().Value == Guid.Empty)
+				{
+					throw new UnauthorizedAccessException();
+				}
+
+				// Role-based authorization
+				IEnumerable<AuthorizeAttribute> authorizeAttributesWithRoles =
+					authorizeAttributes.Where(a => !string.IsNullOrWhiteSpace(a.Roles));
+
+				if (authorizeAttributesWithRoles.Any())
+				{
+					bool authorized = false;
+
+					foreach (string[] roles in authorizeAttributesWithRoles.Select(a => a.Roles.Split(',')))
+					{
+						foreach (string role in roles)
+						{
+							bool isInRole = true;
+							if (isInRole)
+							{
+								authorized = true;
+								break;
+							}
+						}
+					}
+
+					// Must be a member of at least one role in roles
+					if (!authorized)
+					{
+						throw new ForbiddenAccessException();
+					}
+				}
+			}
+
+			//User is authorized / authorization not required
+			return await next();
+		}
+	}
+}
