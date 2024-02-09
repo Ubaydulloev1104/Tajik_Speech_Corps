6ec9207ec2920e058a82a1813fe7464460cbe711
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-09 21:58:17 +0500

    add Text

---
+++ Src/TSR_Api/Domain/Entities/Text.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain.Entities
+{
+    [Index(nameof(Slug))]
+    public class Text : BaseAuditableEntity
+    {
+        public DateTime PublishDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Slug { get; set; }
+
+    }
+}
