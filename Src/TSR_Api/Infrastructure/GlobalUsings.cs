e4192c405a26a96effc77c49610dae7be8ef117b
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-18 22:53:04 +0500

    change code

---
+++ Src/TSR_Api/Infrastructure/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using MediatR;
+global using Application.Common.Interfaces;
+global using Domain.Common;
+global using Microsoft.EntityFrameworkCore;
+global using Domain.Entities;
+global using System.Reflection;
+global using Application.Common.Security;
+global using Infrastructure.Identity.Services;
