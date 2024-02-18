426d6e37e0603e2635804c97c6d558941d625558
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-18 22:06:15 +0500

    Add DeleteApplicationCommandValidator

---
+++ Src/TSR_Api/Application.Contracts/Applications/Commands/Delete/DeleteApplicationCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Contracts.Applications.Commands.Delete
+{
+    public class DeleteApplicationCommandValidator : AbstractValidator<DeleteApplicationCommand>
+    {
+        public DeleteApplicationCommandValidator()
+        {
+            RuleFor(x => x.Slug).NotEmpty();
+        }
+    }
+}
