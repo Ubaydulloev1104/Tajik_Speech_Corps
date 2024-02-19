db1de64c4f4c953ccbdf58570fc8910fa25a9998
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-19 22:29:46 +0500

    add ConfigureServices

---
+++ Src/TSR_Api/TSR_WebUl/ConfigureServices.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json.Serialization;
+using TSR_WebUl.Filters;
+
+namespace TSR_WebUl
+{
+    public static class ConfigureServices
+    {
+        public static void AddWebUiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpContextAccessor();
+
+            services.AddHealthChecks();
+
+            services.AddControllers(options =>
+            {
+                options.Filters.Add<ApiExceptionFilterAttribute>();
+            })
+                .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+            services.AddFluentValidationAutoValidation()
+                .AddFluentValidationClientsideAdapters();
+
+            services.AddValidatorsFromAssembly(typeof(CreateInternshipVacancyCommand).Assembly);
+
+            services.AddRouting(options => options.LowercaseUrls = true);
+            services.AddEndpointsApiExplorer();
+            services.AddCors();
+            services.Configure<ApiBehaviorOptions>(options =>
+                options.SuppressModelStateInvalidFilter = true);
+
+            services.AddOpenApiDocument(configure =>
+            {
+                configure.Title = "MRA.Jobs API";
+                configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Description = "Type into the textbox: Bearer {your JWT token}."
+                });
+
+                configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+            });
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(typeof(BadRequestResponseFilter));
+            });
+            var corsAllowedHosts = configuration.GetSection("CORS").Get<string[]>();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CORS_POLICY", policyConfig =>
+                {
+                    policyConfig.WithOrigins(corsAllowedHosts)
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                });
+            });
+        }
+    }
+}
