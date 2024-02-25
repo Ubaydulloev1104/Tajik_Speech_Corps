b14a2aea9a32d2cd895086f6e7d96e899970aa2c
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-25 23:07:50 +0500

    Add  update

---
+++ Src/TSR_Api/Application.Contracts/MyProfile/Responses/MyProfileResponse.cs
@@ -0,0 +1,21 @@
+﻿using Application.Contracts.Converter.Converter;
+using System.Text.Json.Serialization;
+
+namespace Application.Contracts.MyProfile.Responses
+{
+	public class MyProfileResponse
+	{
+		public string Avatar { get; set; }
+
+		[JsonConverter(typeof(DateTimeToUnixConverter))]
+		public DateTime DateOfBirth { get; set; }
+
+		public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Patronymic { get; set; }
+		public Dtos.Enums.ApplicationStatusDto.Gender Gender { get; set; }
+		public MyIdentityResponse Identity { get; set; }
+	}
+}
