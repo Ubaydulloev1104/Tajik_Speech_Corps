d7bf79c0b76f3a8a7b8ce3c82b4f1531bdb25409
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-25 23:04:22 +0500

    Add application

---
+++ Src/TSR_Api/Application.Contracts/Applications/Commands/CreateApplication/CreateApplicationWithoutApplicantIdCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Contracts.Applications.Commands.CreateApplication
+{
+	public class CreateApplicationWithoutApplicantIdCommand : IRequest<Guid>
+	{
+		public string CoverLetter { get; set; }
+		public Guid Word { get; set; }
+	}
+}
