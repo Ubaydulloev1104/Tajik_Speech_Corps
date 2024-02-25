a00acb942e95f584b46fbd511bf3ca5b5436ecd4
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-25 23:08:45 +0500

    Add My IdentityResponse

---
+++ Src/TSR_Api/Application.Contracts/MyProfile/Responses/MyIdentityResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Contracts.MyProfile.Responses
+{
+	public class MyIdentityResponse
+	{
+		public bool IsActive { get; set; }
+		public IEnumerable<string> Roles { get; set; }
+		public IEnumerable<string> Permissions { get; set; }
+		public bool TwoFactorEnabled { get; set; }
+	}
+}
