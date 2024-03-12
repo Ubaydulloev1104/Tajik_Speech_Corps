846acf26fd7441dc64f1a1272c9caa4f049bf03f
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-03-12 21:47:28 +0500

    commit

---
+++ Src/TSR_Accoun/Src/TSR_Accoun_Application.Contracts/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSR_Accoun_Application.Contracts
+{
+    internal class Class1
+    {
+    }
+}
