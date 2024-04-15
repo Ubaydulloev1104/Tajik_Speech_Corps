f5207ee125d76b18de909529198bef92d1db07e2
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-04-15 12:18:12 +0500

    Add Controller

---
+++ Src/TSR_Api/TSR_WebUl/Controllers/ApiControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TSR_WebUl.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public abstract class ApiControllerBase : ControllerBase
+    {
+        private ISender _mediator;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
--- Src/TSR_Api/TSR_WebUl/Program.cs
+++ Src/TSR_Api/TSR_WebUl/Program.cs
@@ -1,29 +1,79 @@
+using System.Net.Mime;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Application;
+using Infrastructure;
+using Newtonsoft.Json;
+using Sieve.Models;
+using Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using TSR_WebUl;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
-var app = builder.Build();
+builder.Services.AddWebUiServices(builder.Configuration);
 
-// Configure the HTTP request pipeline.
+builder.Services.Configure<SieveOptions>(builder.Configuration.GetSection("Sieve"));
+
+WebApplication app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var initialiser = scope.ServiceProvider.GetRequiredService<DbMigration>();
+    await initialiser.InitialiseAsync();
+}
+
+app.UseHealthChecks("/status", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        var result = new
+        {
+            Status = report.Status.ToString(),
+            Checks = report.Entries.Select(entry => new
+            {
+                Name = entry.Key,
+                Status = entry.Value.Status.ToString(),
+                entry.Value.Description
+            })
+        };
+
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
+    }
+});
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCors("CORS_POLICY");
+app.UseSwaggerUi(settings =>
+{
+    settings.Path = "/api";
+    settings.DocumentPath = "/api/specification.json";
+    settings.EnableTryItOut = true;
+    settings.PersistAuthorization = true;
+});
 
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+
+app.MapGet("/", () => Results.Redirect("/api"));
 
 app.Run();
