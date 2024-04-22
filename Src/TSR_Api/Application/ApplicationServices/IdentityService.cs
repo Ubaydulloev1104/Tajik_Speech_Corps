0876622f04be4c23fcb4ca915628999c5039ccfd
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-04-22 13:57:46 +0500

    Commit

---
+++ Src/TSR_Api/Application/ApplicationServices/IdentityService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using TSR_Accoun_Application.Contracts.Profile.Responses;
+
+namespace Application.ApplicationServices
+{
+    public class IdentityService : IidentityService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IHttpClientFactory _factory;
+
+        public IdentityService(IConfiguration configuration, IHttpContextAccessor httpContextAccessor, IHttpClientFactory factory)
+        {
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+            _factory = factory;
+        }
+
+        public async Task<UserProfileResponse> ApplicantDetailsInfo(string userName = null)
+        {
+            using var identityHttpClient = _factory.CreateClient("IdentityHttpClientProfile");
+            var applicantDetails = new UserProfileResponse();
+
+            var authorizationHeader = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"].ToString();
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                var token = authorizationHeader.Split(' ').Last();
+                identityHttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            using var response = await identityHttpClient.GetAsync(
+                $"{_configuration["MraJobs-IdentityApi:Profile"]}{(userName != null ? $"?userName={userName}" : "")}");
+            if (response.IsSuccessStatusCode)
+            {
+                applicantDetails = await response.Content.ReadFromJsonAsync<UserProfileResponse>();
+            }
+
+            return applicantDetails;
+        }
+    }
+
+}
---
+++ Src/TSR_Api/Application/Common/Interfaces/IidentityService.cs
@@ -0,0 +1,9 @@
+﻿using TSR_Accoun_Application.Contracts.Profile.Responses;
+
+namespace Application.Common.Interfaces
+{
+    public interface IidentityService
+    {
+        Task<UserProfileResponse> ApplicantDetailsInfo(string userName = null);
+    }
+}
