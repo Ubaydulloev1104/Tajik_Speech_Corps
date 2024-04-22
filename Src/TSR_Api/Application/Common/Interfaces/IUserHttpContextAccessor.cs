fa07e841e1f722691961876ab0aa0c5ed0636ab7
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-04-22 17:18:36 +0500

    Fix error

---
+++ Src/TSR_Api/Application/Common/Interfaces/IUserHttpContextAccessor.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Common.Interfaces
+{
+    public interface IUserHttpContextAccessor
+    {
+        bool IsAuthenticated();
+        Guid GetUserId();
+        String GetUserName();
+
+        List<string> GetUserRoles();
+    }
+}
