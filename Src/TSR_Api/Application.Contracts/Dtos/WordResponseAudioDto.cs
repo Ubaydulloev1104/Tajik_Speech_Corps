b0bc163db60f1e0429a4be1fcb0dd4ae28663a92
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-04-23 19:28:40 +0500

    commit

--- Src/TSR_Api/Application.Contracts/Dtos/WordResponseAudioDto.cs
+++ Src/TSR_Api/Application.Contracts/Dtos/WordResponseAudioDto.cs
@@ -6,5 +6,6 @@
         public string FileName { get; set; }
         public string FilePath { get; set; }
         public string Format { get; set; }
+        public string Word { get; set; }
     }
 }
