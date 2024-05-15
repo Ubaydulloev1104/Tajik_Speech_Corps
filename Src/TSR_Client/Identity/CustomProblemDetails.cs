dc5d02d8814a2663f81f318ee68878ca782c02c0
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-05-15 23:03:11 +0500

    Revert "Revert "Revert "commit"""
    This reverts commit e0e85bc88cd52114584b309292dc80ed7c2c95c8.

--- Src/TSR_Client/Identity/CustomProblemDetails.cs
+++ Src/TSR_Client/Identity/CustomProblemDetails.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace TSR_Client.Identity;
-
-public class CustomProblemDetails
+namespace TSR_Client.Identity
 {
-    [JsonPropertyName("status")] public int? Status { get; set; }
+    public class CustomProblemDetails
+    {
+        [JsonPropertyName("status")] public int? Status { get; set; }
 
-    [JsonPropertyName("detail")] public string? Detail { get; set; }
+        [JsonPropertyName("detail")] public string? Detail { get; set; }
 
-    [JsonPropertyName("instance")] public string? Instance { get; set; }
+        [JsonPropertyName("instance")] public string? Instance { get; set; }
 
-    [JsonPropertyName("title")] public string? Title { get; set; }
+        [JsonPropertyName("title")] public string? Title { get; set; }
 
-    [JsonPropertyName("type")] public string? Type { get; set; }
-    [JsonPropertyName("errors")] public Dictionary<string, List<string>>? Errors { get; set; }
+        [JsonPropertyName("type")] public string? Type { get; set; }
+        [JsonPropertyName("errors")] public Dictionary<string, List<string>>? Errors { get; set; }
+    }
 }
