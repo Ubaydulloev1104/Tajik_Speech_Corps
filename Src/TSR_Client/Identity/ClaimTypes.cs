576e4fc329781b59d2450addbe98317486b7ea6f
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-05-15 22:52:38 +0500

    commit

--- Src/TSR_Client/Identity/ClaimTypes.cs
+++ Src/TSR_Client/Identity/ClaimTypes.cs
@@ -1,13 +1,12 @@
-﻿namespace TSR_Client.Identity
-{
-    public static class ClaimTypes
-    {
-        private const string ClaimTypeNamespace = "http://schemas.microsoft.com/ws/2008/06/identity/claims";
+﻿namespace TSR_Client.Identity;
 
-        public const string Application = ClaimTypeNamespace + "/application";
-        public const string Role = ClaimTypeNamespace + "/role";
-        public const string Id = ClaimTypeNamespace + "/id";
-        public const string Username = ClaimTypeNamespace + "/username";
-        public const string Email = ClaimTypeNamespace + "/email";
-    }
+public static class ClaimTypes
+{
+    private const string ClaimTypeNamespace = "http://schemas.microsoft.com/ws/2008/06/identity/claims";
+
+    public const string Application = ClaimTypeNamespace + "/application";
+    public const string Role = ClaimTypeNamespace + "/role";
+    public const string Id = ClaimTypeNamespace + "/id";
+    public const string Username = ClaimTypeNamespace + "/username";
+    public const string Email = ClaimTypeNamespace + "/email";
 }
